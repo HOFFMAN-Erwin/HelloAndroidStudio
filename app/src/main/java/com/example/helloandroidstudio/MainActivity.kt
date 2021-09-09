8b227b3bd2eb7d415fcf1d1216d404ae28a78eeb
Hoffman Erwin <erwinhoffman03@gmail.com>
2021-09-09 16:48:43 -0300

    Variables y Constantes.

--- app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
@@ -7,5 +7,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        variablesYConstantes()
+    }
+    private fun variablesYConstantes (){
+
+        //VARIABLES
+        var myFirstVariable = "Hello hackerman";
+
+        println(myFirstVariable)
+
+        myFirstVariable = "Bienvenides a mi aplicacion"
+
+        println(myFirstVariable)
+
+        var mySecondVariable = "probando segunda variable."
+
+        println(mySecondVariable)
+
+        mySecondVariable = myFirstVariable
+
+        println(mySecondVariable)
+
+        //CONSTANTES
+
+        val myFirstConstant = "Hasta aca vamos muy bien"
+
+        println(myFirstConstant)
+
+        val mySecondConstant:string = myFirstVariable
+
+        printLn(mySecondConstant)
+
     }
 }
