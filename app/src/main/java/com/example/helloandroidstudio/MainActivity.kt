0ff35f5bfc4a74ac7e713212e73ee18428882e17
Hoffman Erwin <erwinhoffman03@gmail.com>
2021-09-10 10:25:49 -0300

    Tipos de datos.

--- app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
@@ -8,9 +8,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        variablesYConstantes()
+        //variablesYConstantes()
+
+        tipoDeDato()
     }
-    private fun variablesYConstantes (){
+   /* private fun variablesYConstantes (){
 
         //VARIABLES
         var myFirstVariable = "Hello hackerman";
@@ -38,6 +40,38 @@ class MainActivity : AppCompatActivity() {
         val mySecondConstant:string = myFirstVariable
 
         printLn(mySecondConstant)
+
+    }*/
+    //tipos de datos
+    private fun tipoDeDato(){
+        //string
+        val myString ="Hola hackerman";
+        val myString2 ="Bienvenides a la aplicacion";
+        val myString3 = myString +" "+ myString2;
+        println(myString3)
+
+       //enteros (byte, short, int, long)
+
+       val myInt = 1
+       val myInt2 = 2
+       val myInt3 = myInt + myInt2
+       println(myInt3)
+
+       //decimales (float,double)
+
+       val myFloat:float= 1.5f
+       val myDouble = 1.5
+       val myDouble2 = 2.6
+       val myDouble3 = 1
+       val myDouble4 = myDouble + myDouble2 + myDouble3
+       println(myDouble4)
+
+       //Boolean
+
+       val myBool = true
+       val myBool2 = false
+       println(myBool==myBool2)
+       println(myBool&&myBool2)
 
     }
 }
