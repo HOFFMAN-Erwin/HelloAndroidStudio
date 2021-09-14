d888f89c29dcf059972f0584f273ae0b96f67d87
Hoffman Erwin <erwinhoffman03@gmail.com>
2021-09-14 17:46:12 -0300

    Bucles for y while.

--- app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
@@ -19,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         arrays()
 
         maps ()
+
+        loops()
     }
     private fun variablesYConstantes (){
 
@@ -227,4 +229,44 @@ class MainActivity : AppCompatActivity() {
         println(myMap)
     }
 
+    private fun loops(){
+
+        val myArray = listOf<String>("Hola","Bienvenidos al loop","Adios")
+        val myMap = mutableMapOf("Erwin" to 1, "Pedro" to 2, "Marcia" to 5)
+
+        //for
+        for (myString in myArray){
+            println(myString)
+        }
+
+        for (myElement in myMap){
+
+            println("${myElement.key}-${myElement.value}")
+
+        }
+        for (x in 0 ..10){
+            println(x)
+        }
+        for (x in 9 until 30){
+            println(x)
+        }
+        for (x in 0 ..10 step 2) {
+            println(x)
+        }
+        for (x in 10 downTo 0 step 3) {
+            println(x)
+        }
+
+        val myNumericArray = (0 .. 20)
+        for (myNum in myNumericArray){
+            println(myNum)
+        }
+
+        //While
+        var x = 0
+        while (x<10){
+            println(x)
+            x += 2
+        }
+    }
 }
